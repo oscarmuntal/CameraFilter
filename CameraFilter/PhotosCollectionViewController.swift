004566f966664570d9cd1f294af3f5823ebd6715
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-13 19:00:38 +0100

    [RxSwift]: Getting the image selected by user at a larger size, checking that the image is not degraded and performing the publishing of the subject

--- CameraFilter/PhotosCollectionViewController.swift
+++ CameraFilter/PhotosCollectionViewController.swift
@@ -12,7 +12,7 @@ import RxSwift
 class PhotosCollectionViewController: UICollectionViewController {
     
     private let selectedPhotoSubject = PublishSubject<UIImage>()
-    public var selectedPhoto: Observable<UIImage> {
+    var selectedPhoto: Observable<UIImage> {
         return selectedPhotoSubject.asObservable()
     }
     
@@ -43,6 +43,23 @@ class PhotosCollectionViewController: UICollectionViewController {
         }
         return cell
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedAsset = self.images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+            
+            guard let info = info else { return }
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage {
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
     
     private func populatePhotos() {
         PHPhotoLibrary.requestAuthorization { [weak self] status in
